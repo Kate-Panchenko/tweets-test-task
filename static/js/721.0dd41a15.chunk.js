49b350e0881da73c485914cb86662e1a9734e379
Kate-Panchenko <110391066+Kate-Panchenko@users.noreply.github.com>
2023-04-26 13:54:54 +0000

    Deploying to gh-pages from @ Kate-Panchenko/tweets-test-task@8c5ab315ef69c34122a0685a25edd1c77067147f 🚀

---
+++ static/js/721.0dd41a15.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktemplate=self.webpackChunktemplate||[]).push([[721],{721:function(e,t,n){n.r(t);var s=n(184);t.default=function(){return(0,s.jsx)("div",{children:(0,s.jsx)("div",{children:"Tweets"})})}}}]);
+//# sourceMappingURL=721.0dd41a15.chunk.js.map
